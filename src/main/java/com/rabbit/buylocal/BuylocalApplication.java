498a68e7f0c65de8fdcc4f4d13ce6d293bb2f97f
shnmkhk <shanmukha.k@gmail.com>
2023-07-30 12:55:51 +0530

    shnmkhk: Added initial commit

---
+++ src/main/java/com/rabbit/buylocal/BuylocalApplication.java
@@ -0,0 +1,13 @@
+package com.rabbit.buylocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuylocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuylocalApplication.class, args);
+	}
+
+}
